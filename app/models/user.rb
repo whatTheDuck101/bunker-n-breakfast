26894c437ea02f4a3eaad83453f9584df2e44535
Tzvi Hamerman <16guitar@gmail.com>
2018-11-13 17:56:29 -0500

    fixed link for sign up button

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :bookings
   has_many :bunkers
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
