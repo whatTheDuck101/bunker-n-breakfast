7dd76877480ea753036709a54b0a46dc7a97af7b
Songyi Han <songyi.lee.han@gmail.com>
2018-11-13 12:03:31 -0500

    doing

--- app/models/bunker.rb
+++ app/models/bunker.rb
@@ -1,9 +1,9 @@
 class Bunker < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  validates :location, presence: true
   validates :name, presence: true
+  validates :location, presence: true
+  validates :price, presence: true, numericality: { only_float: true }
   validates :description, presence: true
   validates :picture, presence: true
-  validates :price, presence: true, numericality: { only_float: true }
 end
