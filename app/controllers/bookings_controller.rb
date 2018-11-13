92ddbd764153abc91534017232c681823b7b5b8e
Vinicius Ishizaki <vinicius.ishizaki@gmail.com>
2018-11-12 19:01:35 -0500

    implemented bookings controller and partially bunkers controller, except for update action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,30 @@
 class BookingsController < ApplicationController
   def index
-
+    @bookings = Booking.all
   end
 
   def new
-
+    @booking = Booking.new
   end
 
   def create
-
+    @booking = Booking.new(booking_params)
+    @booking.bunker = Bunker.find(params[:bunker_id])
+    @booking.user = User.find(params[:user_id])
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
   end
 
   def update
 
   end
 
+  private
+
+  def booking_params
+    params.require(:booking).permit(:status, :user_id, :bunker_id)
+  end
 end
