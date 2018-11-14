283a3b77e5f436d66ceb9baa023d03dd6f85ed85
Vinicius Ishizaki <vinicius.ishizaki@gmail.com>
2018-11-14 12:12:45 -0500

    navbar

--- db/schema.rb
+++ db/schema.rb
@@ -43,12 +43,12 @@ ActiveRecord::Schema.define(version: 2018_11_14_002341) do
     t.string "encrypted_password", default: "", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "photo"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
