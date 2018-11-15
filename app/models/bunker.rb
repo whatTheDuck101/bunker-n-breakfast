7f02f71abdefd1437d0010eac4bd616a6aff8988
Elizabeth Creary <baebaebun101@gmail.com>
2018-11-15 11:51:41 -0500

    Trying to figure out search

--- app/models/bunker.rb
+++ app/models/bunker.rb
@@ -9,4 +9,6 @@ class Bunker < ApplicationRecord
   validates :price, presence: true, numericality: { only_float: true }
   validates :description, presence: true
   # validates :photo, presence: true
+  include PgSearch
+  multisearchable against: [ :address, :price ]
 end
